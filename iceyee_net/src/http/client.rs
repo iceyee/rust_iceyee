fd0b8cac8474f68a868d894e287dfbde606696c9
iceyee <iceyee.studio@qq.com>
2023-11-09 18:46:30 +0800

    iceyee_net 完成Socks5Proxy.

--- iceyee_net/src/http/client.rs
+++ iceyee_net/src/http/client.rs
@@ -238,8 +238,6 @@ impl Proxy for HttpProxy {
                 .header
                 .insert("Proxy-Authorization".to_string(), auth.clone());
         }
-        println!("测试点995");
-        println!("{}", request.to_string());
         plain_socket
             .write(request.to_string().as_bytes())
             .await
@@ -248,9 +246,6 @@ impl Proxy for HttpProxy {
         let response: Response = Response::read_from(&mut plain_socket)
             .await
             .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
-        println!("测试点540");
-        println!("{}", response.to_string());
-        println!("{}", String::from_utf8(response.body.clone()).unwrap());
         if 200 <= response.status_code && response.status_code < 300 {
             // 请求代理连接成功.
         } else {
@@ -305,6 +300,302 @@ impl AsyncRead for HttpProxy {
 }
 
 impl AsyncWrite for HttpProxy {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, StdIoError>> {
+        if self.plain_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.plain_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_write(cx, buf);
+        } else if self.ssl_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.ssl_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_write(cx, buf);
+        } else {
+            return std::task::Poll::Ready(Ok(0));
+        }
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), StdIoError>> {
+        if self.plain_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.plain_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_flush(cx);
+        } else if self.ssl_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.ssl_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_flush(cx);
+        } else {
+            return std::task::Poll::Ready(Ok(()));
+        }
+    }
+
+    fn poll_shutdown(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), StdIoError>> {
+        if self.plain_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.plain_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_shutdown(cx);
+        } else if self.ssl_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.ssl_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_shutdown(cx);
+        } else {
+            return std::task::Poll::Ready(Ok(()));
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Socks5Proxy {
+    target_host: String,
+    target_port: u16,
+    using_ssl: bool,
+    plain_socket: Option<TokioTcpStream>,
+    ssl_socket: Option<TlsStream<TokioTcpStream>>,
+    proxy_host: String,
+    proxy_port: u16,
+    proxy_auth: Option<String>,
+}
+
+impl Socks5Proxy {
+    pub fn new(
+        target_host: &str,
+        target_port: u16,
+        using_ssl: bool,
+        proxy_host: &str,
+        proxy_port: u16,
+        proxy_auth: Option<&str>,
+    ) -> Socks5Proxy {
+        return Socks5Proxy {
+            target_host: target_host.to_string(),
+            target_port: target_port,
+            using_ssl: using_ssl,
+            plain_socket: None,
+            ssl_socket: None,
+            proxy_host: proxy_host.to_string(),
+            proxy_port: proxy_port,
+            proxy_auth: proxy_auth.map(|s| s.to_string()),
+        };
+    }
+}
+
+#[async_trait::async_trait]
+impl Proxy for Socks5Proxy {
+    async fn connect(&mut self) -> Result<(), IceyeeError> {
+        // 1 连接代理.
+        let mut plain_socket: TokioTcpStream =
+            TokioTcpStream::connect((self.proxy_host.clone(), self.proxy_port))
+                .await
+                .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+        // 2 认证.
+        // client:
+        // +----+----------+----------+
+        // |VER | NMETHODS | METHODS  |
+        // +----+----------+----------+
+        // | 1  |    1     | 1 to 255 |
+        // +----+----------+----------+
+        // server
+        // +----+--------+
+        // |VER | METHOD |
+        // +----+--------+
+        // | 1  |    1   |
+        // +----+--------+
+        // The values currently defined for METHOD are:
+        //   o X’00’ NO AUTHENTICATION REQUIRED
+        //   o X’01’ GSSAPI
+        //   o X’02’ USERNAME/PASSWORD
+        //   o X’03’ to X’7F’ IANA ASSIGNED
+        //   o X’80’ to X’FE’ RESERVED FOR PRIVATE METHODS
+        //   o X’FF’ NO ACCEPTABLE METHODS
+        plain_socket
+            .write(&[0x05u8, 0x02, 0x00, 0x02])
+            .await
+            .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+        let mut buffer: [u8; 0xFFF] = [0; 0xFFF];
+        let length: usize = plain_socket
+            .read(&mut buffer)
+            .await
+            .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+        if length != 2 {
+            return Err(IceyeeError::from("非预期."));
+        }
+        if buffer[1] == 0xFF {
+            // 认证被拒绝.
+            return Err(IceyeeError::from("代理认证被拒绝."));
+        } else if buffer[1] == 0x02 {
+            // USERNAME/PASSWORD.
+            // client:
+            // +----+------+----------+------+----------+
+            // |VER | ULEN |  UNAME   | PLEN |  PASSWD  |
+            // +----+------+----------+------+----------+
+            // | 1  |  1   | 1 to 255 |  1   | 1 to 255 |
+            // +----+------+----------+------+----------+
+            // VER: 0x01
+            if self.proxy_auth.is_some() && self.proxy_auth.as_ref().unwrap().contains(":") {
+                let auth: String = self.proxy_auth.as_ref().unwrap().clone();
+                let mut a001 = auth.splitn(2, ":");
+                let username: String = a001.next().unwrap().to_string();
+                let username: &[u8] = username.as_bytes();
+                let password: String = a001.next().unwrap().to_string();
+                let password: &[u8] = password.as_bytes();
+                let mut auth: Vec<u8> = Vec::new();
+                auth.push(0x01);
+                auth.push(username.len() as u8);
+                auth.extend_from_slice(username);
+                auth.push(password.len() as u8);
+                auth.extend_from_slice(password);
+                plain_socket
+                    .write(&auth)
+                    .await
+                    .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+                // server:
+                // +----+--------+
+                // |VER | STATUS |
+                // +----+--------+
+                // | 1  |   1    |
+                // +----+--------+
+                // 返回STATUS, 0x00表示成功.
+                let length: usize = plain_socket
+                    .read(&mut buffer)
+                    .await
+                    .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+                if length != 2 {
+                    return Err(IceyeeError::from("非预期."));
+                } else if buffer[1] != 0x00 {
+                    return Err(IceyeeError::from("代理认证失败."));
+                }
+            }
+        }
+        // 3 CONNECT.
+        // client:
+        // +----+-----+-------+------+----------+----------+
+        // |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+        // +----+-----+-------+------+----------+----------+
+        // | 1  |  1  | X’00’ |  1   | Variable |     2    |
+        // +----+-----+-------+------+----------+----------+
+        // o VER protocol version: X’05’
+        // o CMD
+        //   o CONNECT X’01’
+        //   o BIND X’02’
+        //   o UDP ASSOCIATE X’03’
+        // o RSV RESERVED
+        // o ATYP address type of following address
+        //   o IP V4 address: X’01’
+        //   o DOMAINNAME: X’03’    1*(长度) + *(地址)
+        //   o IP V6 address: X’04’
+        // o DST.ADDR desired destination address
+        // o DST.PORT desired destination port in network octet order 大端序
+        let mut request: Vec<u8> = Vec::new();
+        request.push(0x05);
+        request.push(0x01);
+        request.push(0x00);
+        request.push(0x03);
+        request.push(self.target_host.len() as u8);
+        request.extend_from_slice(self.target_host.as_bytes());
+        request.push(((self.target_port >> 8) & 0xFF) as u8);
+        request.push(((self.target_port >> 0) & 0xFF) as u8);
+        plain_socket
+            .write(&request)
+            .await
+            .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+        // server:
+        // +----+-----+-------+------+----------+----------+
+        // |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+        // +----+-----+-------+------+----------+----------+
+        // | 1  |  1  | X’00’ |  1   | Variable |    2     |
+        // +----+-----+-------+------+----------+----------+
+        // o VER protocol version: X’05’
+        // o REP Reply field:
+        //   o X’00’ succeeded
+        //   o X’01’ general SOCKS server failure
+        //   o X’02’ connection not allowed by ruleset
+        //   o X’03’ Network unreachable
+        //   o X’04’ Host unreachable
+        //   o X’05’ Connection refused
+        //   o X’06’ TTL expired
+        //   o X’07’ Command not supported
+        //   o X’08’ Address type not supported
+        //   o X’09’ to X’FF’ unassigned
+        // o RSV RESERVED
+        // o ATYP address type of following address
+        //   o IP V4 address: X’01’
+        //   o DOMAINNAME: X’03’
+        //   o IP V6 address: X’04’
+        // o BND.ADDR server bound address
+        // o BND.PORT server bound port in network octet order
+        let length: usize = plain_socket
+            .read(&mut buffer)
+            .await
+            .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+        if length < 4 {
+            return Err(IceyeeError::from("非预期."));
+        }
+        if buffer[1] == 0x00 {
+            // succeeded.
+        } else if buffer[1] == 0x01 {
+            return Err(IceyeeError::from("general SOCKS server failure"));
+        } else if buffer[1] == 0x02 {
+            return Err(IceyeeError::from("connection not allowed by ruleset"));
+        } else if buffer[1] == 0x03 {
+            return Err(IceyeeError::from("Network unreachable"));
+        } else if buffer[1] == 0x04 {
+            return Err(IceyeeError::from("Host unreachable"));
+        } else if buffer[1] == 0x05 {
+            return Err(IceyeeError::from("Connection refused"));
+        } else if buffer[1] == 0x06 {
+            return Err(IceyeeError::from("TTL expired"));
+        } else if buffer[1] == 0x07 {
+            return Err(IceyeeError::from("Command not supported"));
+        } else if buffer[1] == 0x08 {
+            return Err(IceyeeError::from("Address type not supported"));
+        } else if buffer[1] == 0x09 {
+            return Err(IceyeeError::from("to X’FF’ unassigned"));
+        }
+        // 4 tls握手.
+        if !self.using_ssl {
+            self.plain_socket = Some(plain_socket);
+        } else {
+            let connector = tokio_native_tls::native_tls::TlsConnector::new()
+                .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+            let connector = tokio_native_tls::TlsConnector::from(connector);
+            let ssl_socket: TlsStream<TokioTcpStream> = connector
+                .connect(self.target_host.as_str(), plain_socket)
+                .await
+                .map_err(|e| IceyeeError::from(Box::new(e) as Box<dyn StdError>))?;
+            self.ssl_socket = Some(ssl_socket);
+        }
+        Ok(())
+    }
+
+    fn close(&mut self) {
+        self.plain_socket = None;
+        self.ssl_socket = None;
+        return;
+    }
+
+    fn is_closed(&self) -> bool {
+        self.plain_socket.is_none() && self.ssl_socket.is_none()
+    }
+}
+
+impl AsyncRead for Socks5Proxy {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<Result<(), StdIoError>> {
+        if self.plain_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.plain_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_read(cx, buf);
+        } else if self.ssl_socket.is_some() {
+            let mut pinned = std::pin::pin!(self.ssl_socket.as_mut().unwrap());
+            return pinned.as_mut().poll_read(cx, buf);
+        } else {
+            return std::task::Poll::Ready(Ok(()));
+        }
+    }
+}
+
+impl AsyncWrite for Socks5Proxy {
     fn poll_write(
         mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
